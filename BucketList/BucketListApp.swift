d663bbf7cd55bf8207f9904ec8292a194deb2c50
Daniel JCH <danielj.callisayah@gmail.com>
2022-04-14 12:22:24 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 14/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
