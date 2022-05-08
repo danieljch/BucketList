f3c5bffd5886e47b06525063f539a24a42dd23d4
Daniel JCH <danielj.callisayah@gmail.com>
2022-05-08 19:51:33 -0400

    Comparable for custom types

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct User: Identifiable, Comparable {
+        let id = UUID()
+        let firstName: String
+        let lastName: String
+
+        static func <(lhs: User, rhs: User) -> Bool {
+            lhs.lastName < rhs.lastName
+        }
+    }
+    
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(users) { user in
+                    Text("\(user.lastName), \(user.firstName)")
+                }
     }
 }
 
