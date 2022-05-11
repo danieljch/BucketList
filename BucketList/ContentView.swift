ad2c44b193a713fbb01e989f9895cca6daf01b58
Daniel JCH <danielj.callisayah@gmail.com>
2022-05-11 17:55:40 -0400

    MapKit with SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,25 +6,39 @@
 //
 
 import SwiftUI
+import MapKit
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+	
 struct ContentView: View {
-    struct User: Identifiable, Comparable {
-        let id = UUID()
-        let firstName: String
-        let lastName: String
-
-        static func <(lhs: User, rhs: User) -> Bool {
-            lhs.lastName < rhs.lastName
-        }
-    }
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted()
     
     var body: some View {
+        let locations = [
+            Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+            Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+        ]
+        
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
+        }
+       
         Text("Hello World")
             .onTapGesture {
                 let str = "Test Message"
